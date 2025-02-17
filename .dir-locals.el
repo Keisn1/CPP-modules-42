f0c7d1f462031f15a4fc1a142109e654e25c24c8
Kay Freyer <kay.freyer@icloud.com>
2025-02-17 07:46:16 +0100

    start exploring CPP

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((nil . ((org-roam-directory . "~/workspace/cpp-module-00/roam")
+         (org-roam-db-location . "~/workspace/cpp-module-00/roam/cpp_modules.db"))))
---
+++ ex00/megaphone.cpp
@@ -0,0 +1,1 @@
+#include "megaphone.h"

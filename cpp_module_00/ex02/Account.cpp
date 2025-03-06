a1c02287427636c3fb3da2badc6c6b567e590157
Kay Freyer <kay.freyer@icloud.com>
2025-03-06 16:09:04 +0100

    finish ex02

--- cpp_module_00/ex02/Account.cpp
+++ cpp_module_00/ex02/Account.cpp
@@ -18,7 +18,7 @@ Account::Account(int initial_deposit) {
 
 	_displayTimestamp();
 	std::cout << " index:" << _accountIndex << ";amount:" << _amount
-			  << ";created" << std::endl;
+						<< ";created" << std::endl;
 
 }
 
@@ -30,7 +30,7 @@ Account::~Account() {
 bool Account::makeWithdrawal(int withdrawal) {
 	_displayTimestamp();
 	std::cout << " index:" << _accountIndex << ";p_amount:"
-			  << _amount << ";withdrawal:";
+						<< _amount << ";withdrawal:";
 	if (withdrawal > _amount) {
 		std::cout << "refused" << std::endl;
 		return false;
@@ -41,7 +41,7 @@ bool Account::makeWithdrawal(int withdrawal) {
 	_totalAmount -= withdrawal;
 
 	std::cout << withdrawal << ";amount:" << _amount
-			  << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+						<< ";nb_withdrawals:" << _nbWithdrawals << std::endl;
 
 	return true;
 }
@@ -55,34 +55,34 @@ void Account::makeDeposit(int deposit) {
 
 	_displayTimestamp();
 	std::cout << " index:"
-			  << _accountIndex
-			  << ";p_amount:" << p_amount
-			  << ";deposit:" << deposit
-			  << ";amount:" << _amount
-			  << ";nb_deposits:" << _nbDeposits  << std::endl;
+						<< _accountIndex
+						<< ";p_amount:" << p_amount
+						<< ";deposit:" << deposit
+						<< ";amount:" << _amount
+						<< ";nb_deposits:" << _nbDeposits  << std::endl;
 }
 
 void Account::displayAccountsInfos(void) {
 	_displayTimestamp();
 	std::cout << " accounts:" << _nbAccounts << ";total:" << _totalAmount
-			  << ";deposits:" << _totalNbDeposits
-			  << ";withdrawals:" << _totalNbWithdrawals << std::endl;
+						<< ";deposits:" << _totalNbDeposits
+						<< ";withdrawals:" << _totalNbWithdrawals << std::endl;
 };
 
 void Account::displayStatus(void) const {
 	_displayTimestamp();
 	std::cout << " index:" << _accountIndex << ";amount:" << _amount
-			  << ";deposits:" << _nbDeposits
-			  << ";withdrawals:" << _nbWithdrawals << std::endl;
+						<< ";deposits:" << _nbDeposits
+						<< ";withdrawals:" << _nbWithdrawals << std::endl;
 };
 
 void Account::_displayTimestamp(void) {
 	std::time_t now = std::time(NULL);
 	std::tm* now_tm = std::localtime(&now);  // Or std::gmtime(&now) for UTC
 
-    char buffer[20];
-    std::strftime(buffer, sizeof(buffer), "[%Y%m%d_%H%M%S]", now_tm);
-    std::cout << buffer;
+	char buffer[20];
+	std::strftime(buffer, sizeof(buffer), "[%Y%m%d_%H%M%S]", now_tm);
+	std::cout << buffer;
 }
 
 int Account::getNbAccounts(void) {

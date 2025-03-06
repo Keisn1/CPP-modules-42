244ce84d205db8f28ec868f6c6a0404177af2a65
Kay Freyer <kay.freyer@icloud.com>
2025-03-06 18:42:03 +0100

    begin ex01

---
+++ cpp_module_01/ex01/src/Zombie.cpp
@@ -0,0 +1,10 @@
+#include "Zombie.hpp"
+#include <iostream>
+
+Zombie::Zombie(std::string name) : name(name) {}
+
+Zombie::~Zombie() {
+	 std::cout << name << " got destroyed" << std::endl;
+}
+
+void Zombie::announce(void) { std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl; }

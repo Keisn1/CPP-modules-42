03d36b12460b11b55de6268c79cce53905f80f43
Kay Freyer <kfreyer@c1r6s4.42wolfsburg.de>
2025-03-16 10:23:19 +0100

    add info comment megaphone

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -19,8 +19,15 @@ int main(int argc, char* argv[]) {
         std::cout << "* LOUD AND UNBEREABLE FEEDBACK NOISE *";
     } else {
         for (int i = 1; i < argc; i++) {
-            std::string str = argv[i];
+			// creates a std::string object =str= and initializes it with the C-style string argv[i].
+			// The std::string constructor is used to handle the conversion from a C-style string to a C++ string
+            std::string str = argv[i]; //
             for (size_t j = 0; j < str.length(); j++)
+			    // https://en.cppreference.com/w/cpp/string/byte/toupper
+			    // std::toupper expects an argument that fits the unsigned char range.
+			    // undefined if the argument's value is neither representable as unsigned char nor equal to EOF
+			    // the return type of std::toupper is int, therefore another cast
+			    // To use these functions safely with plain chars (or signed chars), the argument should first be converted to unsigned char
                 std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(str[j])));
         }
     }

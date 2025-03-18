5a8fb7723149ea2fa395a11422cfe3b8a5ee83b8
Kay Freyer <kay.freyer@icloud.com>
2025-03-18 07:14:40 +0100

    Change the output of Harl

--- cpp_module_01/ex05/src/Harl.cpp
+++ cpp_module_01/ex05/src/Harl.cpp
@@ -3,26 +3,18 @@
 #include <cstring> // For strcmp
 
 void Harl::debug() {
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!" << std::endl;
+    std::cout << "[DEBUG]" << std::endl << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info() {
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+    std::cout << "[INFO]" << std::endl << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 void Harl::warning() {
-    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month."<< std::endl;
+    std::cout << "[WARNING]" << std::endl << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month."<< std::endl;
 }
 
 void Harl::error() {
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
-}
-
-int Harl::get_debug_level(std::string level) {
-    if (level == "DEBUG") return DEBUG;
-    if (level == "INFO") return INFO;
-    if (level == "WARNING") return WARNING;
-    if (level == "ERROR") return ERROR;
-    return DEBUG;
+    std::cout << "[ERROR]" << std::endl << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void Harl::complain(std::string level) {
@@ -55,5 +47,5 @@ void Harl::complain(std::string level) {
             return;
         }
     }
-    std::cout << "Unknown log level: " << level << std::endl;
+    std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
--- cpp_module_01/ex05/tests/test_main.cpp
+++ cpp_module_01/ex05/tests/test_main.cpp
@@ -12,30 +12,30 @@ TEST(harlTests, firstTests) {
 	testing::internal::CaptureStdout();
 	harl.complain("DEBUG");
 	std::string got = testing::internal::GetCapturedStdout();
-	std::string want = "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!\n";
+	std::string want =  "[DEBUG]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!\n";
 	ASSERT_STREQ(want.c_str(), got.c_str());
 
 	testing::internal::CaptureStdout();
 	harl.complain("INFO");
 	got = testing::internal::GetCapturedStdout();
-	want = "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+	want = "[INFO]\nI cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
 	ASSERT_STREQ(want.c_str(), got.c_str());
 
 	testing::internal::CaptureStdout();
 	harl.complain("WARNING");
 	got = testing::internal::GetCapturedStdout();
-	want = "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.\n";
+	want = "[WARNING]\nI think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.\n";
 	ASSERT_STREQ(want.c_str(), got.c_str());
 
 	testing::internal::CaptureStdout();
 	harl.complain("ERROR");
 	got = testing::internal::GetCapturedStdout();
-	want = "This is unacceptable! I want to speak to the manager now.\n";
+	want = "[ERROR]\nThis is unacceptable! I want to speak to the manager now.\n";
 	ASSERT_STREQ(want.c_str(), got.c_str());
 
 	testing::internal::CaptureStdout();
 	harl.complain("something else");
 	got = testing::internal::GetCapturedStdout();
-	want = "Unknown log level: something else\n";
+	want = "[ Probably complaining about insignificant problems ]\n";
 	ASSERT_STREQ(want.c_str(), got.c_str());
 }

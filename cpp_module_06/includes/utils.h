3ec526dc62e7b396516c52fa55bccc40c3d73403
Kay Freyer <kay.freyer@icloud.com>
2025-07-05 09:07:23 +0200

    ex00 cpp06

--- cpp_module_06/includes/utils.h
+++ cpp_module_06/includes/utils.h
@@ -3,5 +3,10 @@
 
 #include <string>
 bool isConvertibleToInt(std::string str);
+bool isConvertibleToFloat(std::string str);
+bool isConvertibleToDouble(std::string str);
+
+bool checkPseudo(std::string& str);
+double convertPseudo(std::string& str);
 
 #endif // UTILS_H
--- cpp_module_06/src/ScalarConverter.cpp
+++ cpp_module_06/src/ScalarConverter.cpp
@@ -12,9 +12,38 @@ ScalarConverter::ScalarConverter(const ScalarConverter&) {}
 ScalarConverter& ScalarConverter::operator=(const ScalarConverter&) { return *this; }
 
 void ScalarConverter::convert(std::string str) {
-    if (isConvertibleToInt(str)) {
-        std::cout << "is convertible" << std::endl;
+    std::cout << str.length() << std::endl;
+    std::istringstream iss(str);
+    char c;
+    iss >> c;
+    if (iss.eof() && !iss.fail()) {
+        std::cout << "not convertible to char" << std::endl;
     } else {
-        std::cout << "not convertible" << std::endl;
+        std::cout << "is convertible to char" << std::endl;
+        std::cout << static_cast< int >(c) << std::endl;
+    }
+    // if (checkPseudo(str)) {
+    // }
+
+    if (str[str.length() - 1] == 'f') {
+        str = str.substr(0, str.length() - 1);
+    }
+
+    if (isConvertibleToInt(str)) {
+        std::cout << "is convertible to int" << std::endl;
+    } else {
+        std::cout << "not convertible to int" << std::endl;
+    }
+
+    if (isConvertibleToFloat(str)) {
+        std::cout << "is convertible to float" << std::endl;
+    } else {
+        std::cout << "not convertible to float" << std::endl;
+    }
+
+    if (isConvertibleToDouble(str)) {
+        std::cout << "is convertible to double" << std::endl;
+    } else {
+        std::cout << "not convertible to double" << std::endl;
     }
 };

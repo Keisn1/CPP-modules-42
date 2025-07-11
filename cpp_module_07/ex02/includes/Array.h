6eb4009039e2d9760edff30b5815f11556882bfd
Kay Freyer <kay.freyer@icloud.com>
2025-07-11 07:48:58 +0200

    working on ex02

--- cpp_module_07/ex02/includes/Array.h
+++ cpp_module_07/ex02/includes/Array.h
@@ -1,31 +1,59 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
-#include <ostream>
-
 #include <cstddef>
+#include <exception>
+#include <iostream>
 
 template < typename T >
 class Array {
   private:
-    size_t _len;
+    size_t _size;
+    T* _data;
 
   public:
-    Array(void) : _len(0) {}
-    Array(unsigned int n) : _len(n) { T _data[] = new T[n]; };
-    ~Array(void);
-    Array(const Array&);
-    Array& operator=(const Array&);
-    size_t length();
-};
+    Array(void) : _size(0), _data(NULL) {}
 
-/* template <typename T> */
-/* std::ostream& operator<<(std::ostream& out, const Array&) { */
-/*     // for (size_t i = 0; i < a.length(); i++) { */
-/*     //     out << a[i]; */
-/*     // } */
-/*     // out << std::endl; */
-/*     return out; */
-/* } */
+    Array(unsigned int n) : _size(n), _data(NULL) {
+        try {
+            this->_data = new T[n];
+        } catch (std::exception& e) {
+            std::cerr << e.what() << std::endl;
+            _size = 0;
+            return;
+        }
+
+        T* tmp = new T();
+        for (size_t i = 0; i < _size; i++) {
+            _data[i] = *tmp;
+        }
+        delete tmp;
+    };
+
+    ~Array(void) {
+        if (this->_data) {
+            delete[] this->_data;
+        }
+    };
+
+    // Array(const Array& arr) {
+    //     try {
+    //         this->_data = new T[arr._size];
+    //     } catch (std::exception& e) {
+    //         std::cerr << e.what() << std::endl;
+    //         return;
+    //     }
+
+    //     _size = arr._size;
+    //     for (size_t i = 0; i < arr._size; i++) {
+    //         _data[i] = arr._data[i];
+    //     }
+    // };
+
+    Array& operator=(const Array&);
+    T& operator[](int index) { return _data[index]; }
+
+    size_t size() { return _size; };
+};
 
 #endif // ARRAY_H
--- cpp_module_07/ex02/src/main.cpp
+++ cpp_module_07/ex02/src/main.cpp
@@ -1,8 +1,87 @@
 #include "Array.h"
 #include <iostream>
+#include <limits>
+
+class TestClass {
+  private:
+    int n;
+
+  public:
+    TestClass() : n(1) {};
+    ~TestClass() {};
+    void print() { std::cout << n; }
+};
 
 int main() {
-    Array* a = new Array();
-    std::cout << *a << std::endl;
+    {
+        std::cout << "(TEST 1)" << std::endl;
+        std::cout << "Construct a new array with no parameter and different types" << std::endl;
+        {
+            Array< char >* a = new Array< char >();
+            delete a;
+        }
+        {
+            Array< int >* a = new Array< int >();
+            delete a;
+        }
+        {
+            Array< std::string >* a = new Array< std::string >();
+            delete a;
+        }
+        {
+            Array< TestClass >* a = new Array< TestClass >();
+            delete a;
+        }
+    }
+    {
+        std::cout << "(TEST 2)" << std::endl;
+        std::cout << "Construct a new array with parameter n and different types" << std::endl;
+        std::cout << "Then print the default values of the parameter" << std::endl;
+        {
+            Array< char >* a = new Array< char >(10);
+            std::cout << static_cast< int >((*a)[0]) << std::endl;
+            delete a;
+        }
+        {
+            Array< int >* a = new Array< int >(10);
+            // for (size_t i = 0; i < a->size(); i++) {
+            //     std::cout << (*a)[0] << std::endl;
+            // }
+            delete a;
+        }
+        {
+            Array< std::string >* a = new Array< std::string >(std::numeric_limits< unsigned int >::max());
+            delete a;
+        }
+        // {
+        //     Array< TestClass >* a = new Array< TestClass >(20);
+        //     delete a;
+        // }
+    }
+    // {
+    //     std::cout << "(TEST 3)" << std::endl;
+    //     std::cout << "Copy construct and look that nothing changes" << std::endl;
+    //     std::cout << "Then print the default values of the parameter" << std::endl;
+    //     {
+    //         Array< char >* a = new Array< char >(10);
+    //         delete a;
+    //     }
+    //     {
+    //         Array< int >* a = new Array< int >(1);
+    //         delete a;
+    //     }
+    //     {
+    //         Array< std::string >* a = new Array< std::string >(std::numeric_limits< unsigned int >::max());
+    //         delete a;
+    //     }
+    //     {
+    //         Array< TestClass >* a = new Array< TestClass >(20);
+    //         delete a;
+    //     }
+    // }
+    // {
+    //     int* a = new int();
+    //     std::cout << *a << std::endl;
+    // }
     return 0;
 }

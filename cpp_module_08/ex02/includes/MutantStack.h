152ddb7a5bea122cc4f47fe48c0b9237f6d4b5fe
Kay Freyer <kay.freyer@icloud.com>
2025-08-03 20:47:58 +0200

    finish MutantStack

--- cpp_module_08/ex02/includes/MutantStack.h
+++ cpp_module_08/ex02/includes/MutantStack.h
@@ -1,24 +1,45 @@
 #ifndef MUTANTSTACK_H
 #define MUTANTSTACK_H
 
+#include <deque>
+#include <iostream>
 #include <stack>
+#include <typeinfo> // for testing purposes
 
-template < typename T >
-class MutantStack {
-  private:
-    std::stack< T > _stack;
-
+template < typename T, class Container = std::deque< T > >
+class MutantStack
+    : public std::stack< T, Container > { // without populating the Container, the last test in main will not compile
   public:
-    MutantStack(void) : _stack(std::stack< T >()) {};
+    MutantStack(void) {};
+
     ~MutantStack(void) {};
-    MutantStack(const MutantStack< T >& other) : _stack(other._stack) {};
-    MutantStack< T >& operator=(const MutantStack< T >& other) {
-        if (*this == other) {
+
+    MutantStack(const MutantStack< T, Container >& other) : std::stack< T, Container >(other) {};
+
+    MutantStack< T, Container >& operator=(const MutantStack< T, Container >& other) {
+        if (this == &other) {
             return *this;
         }
-        this->_stack = other._stack;
+        this->c = other.c;
         return *this;
     };
+
+    typedef typename Container::iterator iterator;
+    typedef typename Container::const_iterator const_iterator;
+    typedef typename Container::reverse_iterator reverse_iterator;
+    typedef typename Container::const_reverse_iterator const_reverse_iterator;
+
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
+    const_iterator begin() const { return this->c.begin(); }
+    const_iterator end() const { return this->c.end(); }
+    reverse_iterator rbegin() { return this->c.rbegin(); }
+    reverse_iterator rend() { return this->c.rend(); }
+    const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+    const_reverse_iterator rend() const { return this->c.rend(); }
+
+    // for testing purposes
+    void showContainerType() { std::cout << "Container type: " << typeid(this->c).name() << std::endl; }
 };
 
 #endif // MUTANTSTACK_H
--- cpp_module_08/ex02/src/main.cpp
+++ cpp_module_08/ex02/src/main.cpp
@@ -1,29 +1,167 @@
 #include "MutantStack.h"
 #include <iostream>
+#include <list>
 #include <stack>
+#include <vector>
 
 int main() {
     {
         std::cout << "(TEST 1)" << std::endl;
-        std::cout << "Stack default construction" << std::endl;
+        std::cout << "Construction / push-pop-top / copy construction / copy assignment" << std::endl;
+
         {
-            std::stack< int > aStack;
-            std::stack< int >* aStackp = new std::stack< int >();
-            delete aStackp;
+            MutantStack< int, std::list< int > > mStack;
+            mStack.showContainerType();
+            mStack.push(2);
+            mStack.push(17);
+            std::cout << mStack.top() << std::endl;
+
+            mStack.pop();
+            std::cout << mStack.top() << std::endl;
+            std::cout << mStack.size() << std::endl;
+            mStack.push(3);
+            mStack.push(5);
+            mStack.push(737);
+            std::cout << mStack.size() << std::endl;
+
+            std::cout << "using copy constructor" << std::endl;
+            MutantStack< int, std::list< int > > mcStack(mStack);
+            mcStack.showContainerType();
+
+            std::cout << mcStack.size() << std::endl;
+            while (!mcStack.empty()) {
+                mcStack.pop();
+            }
+            std::cout << mStack.size() << std::endl;
+
+            std::cout << "using copy-assing constructor" << std::endl;
+            MutantStack< int, std::list< int > > mcaStack;
+            mcaStack.showContainerType();
+            mcaStack = mStack;
+            while (!mcaStack.empty()) {
+                std::cout << mcaStack.top() << std::endl;
+                mcaStack.pop();
+            }
+            std::cout << mStack.size() << std::endl;
         }
+    }
+    std::cout << std::endl;
+
+    {
+        std::cout << "(TEST 1.2) with other container type" << std::endl;
+        std::cout << "Construction / push-pop-top / copy construction / copy assignment" << std::endl;
+
         {
-            MutantStack< int > aMutant;
-            MutantStack< int >* aMutantP = new MutantStack< int >();
-            delete aMutantP;
+            MutantStack< int, std::vector< int > > mStack;
+            mStack.showContainerType();
+            mStack.push(2);
+            mStack.push(17);
+            std::cout << mStack.top() << std::endl;
+
+            mStack.pop();
+            std::cout << mStack.top() << std::endl;
+            std::cout << mStack.size() << std::endl;
+            mStack.push(3);
+            mStack.push(5);
+            mStack.push(737);
+            std::cout << mStack.size() << std::endl;
+            MutantStack< int, std::vector< int > > mcStack(mStack);
+            std::cout << mcStack.size() << std::endl;
+            while (!mcStack.empty()) {
+                mcStack.pop();
+            }
+            std::cout << mStack.size() << std::endl;
+
+            MutantStack< int, std::vector< int > > mcaStack;
+            mcaStack = mStack;
+            while (!mcaStack.empty()) {
+                std::cout << mcaStack.top() << std::endl;
+                mcaStack.pop();
+            }
+            std::cout << mStack.size() << std::endl;
+        }
+    }
+    {
+        std::cout << "(TEST 2)" << std::endl;
+        std::cout << "iterator" << std::endl;
+        {
+
+            MutantStack< int > mStack;
+            mStack.push(2);
+            mStack.push(17);
+            mStack.push(3);
+            mStack.push(5);
+            mStack.push(737);
+
+            MutantStack< int >::iterator it = mStack.begin();
+            MutantStack< int >::iterator ite = mStack.end();
+            it++;
+            it--;
+            while (it != ite) {
+                std::cout << *it << std::endl;
+                ++it;
+            }
+            std::stack< int > s(mStack);
         }
     }
     std::cout << std::endl;
     {
-        std::cout << "(TEST 2)" << std::endl;
-        std::cout << "Copy Constructor and Copy assignment" << std::endl;
-        // {
-        // 	MutantStack<int>
+        std::cout << "(TEST 2.1)" << std::endl;
+        std::cout << "reverse iterator" << std::endl;
+        {
 
-        // }
+            MutantStack< int > mStack;
+            mStack.push(2);
+            mStack.push(17);
+            mStack.push(3);
+            mStack.push(5);
+            mStack.push(737);
+
+            MutantStack< int >::reverse_iterator it = mStack.rbegin();
+            MutantStack< int >::reverse_iterator ite = mStack.rend();
+            it++;
+            it--;
+            while (it != ite) {
+                std::cout << *it << std::endl;
+                ++it;
+            }
+            std::stack< int > s(mStack);
+        }
+    }
+    std::cout << std::endl;
+    {
+        std::cout << "(TEST 2.2)" << std::endl;
+        std::cout << "const iterator" << std::endl;
+        {
+
+            MutantStack< int > mStack;
+            mStack.push(2);
+            mStack.push(17);
+            mStack.push(3);
+            mStack.push(5);
+            mStack.push(737);
+
+            const MutantStack< int > mStackConst = mStack;
+
+            MutantStack< int >::const_iterator it = mStackConst.begin();
+            MutantStack< int >::const_iterator ite = mStackConst.end();
+            it++;
+            it--;
+            while (it != ite) {
+                std::cout << *it << std::endl;
+                ++it;
+            }
+            std::stack< int > s(mStack);
+        }
+    }
+    {
+
+        MutantStack< int, std::vector< int > > ms;
+        ms.push(1);
+        ms.push(2);
+
+        // Try to use iterators - this should fail or behave incorrectly
+        MutantStack< int, std::vector< int > >::iterator it = ms.begin();
+        (void)it;
     }
 }
